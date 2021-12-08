c15875bf2981f113a35ec01ce676b56dea929a46
ivan anton <ivanantonsagastizabal@gmail.com>
2021-12-08 12:11:50 +0100

    Ended search engine

--- SearchEngine/app/src/main/java/ivan/SearchEngine/MainActivity.java
+++ SearchEngine/app/src/main/java/ivan/SearchEngine/MainActivity.java
@@ -2,7 +2,15 @@ package ivan.SearchEngine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText queryEditText = findViewById(R.id.query);
+        Button searchButton = findViewById(R.id.button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String query = URLEncoder.encode(queryEditText.getText().toString(), "UTF-8");
+                    Uri uri = Uri.parse("http://www.google.com/search?q=" + query);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
